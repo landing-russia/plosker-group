f16f271ecbab10057d9a1f15a9f38dd4c287b056
landing-russia <landing-russia@yandex.ru>
2021-11-18 08:06:14 +0300

    header continue

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,16 +8,16 @@
 
 <script>
   import "../app.css";
-  import Nav from "$lib/Nav.svelte";
+  // import Nav from "$lib/Nav.svelte";
   import Footer from "$lib/Footer.svelte";
   import PageTransition from "$lib/PageTransition.svelte";
   export let key;
 </script>
 
 <div
-  class="bg-gray-100 flex flex-col min-h-screen font-ptsans selection:bg-blueGray-300"
+  class="bg-gray-100 flex flex-col min-h-screen font-ptsans selection:bg-blueGray-200"
 >
-  <Nav />
+  <!-- <Nav /> -->
   <main class="flex-grow w-full">
     <PageTransition refresh={key}>
       <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,48 @@
-<main class="lg:relative">
-  <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
-    <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-      <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-        <span class="block xl:inline">Data to enrich your</span>
-        <span class="block text-indigo-600 xl:inline">online business</span>
+<div class="lg:relative">
+  <!-- <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left"> -->
+  <div class="relative flex items-center lg:h-screen">
+    <div
+      class="lg:absolute w-1/2 flex items-center justify-between lg:top-8 lg:left-0 px-3 xs:px-4 sm:px-6 lg:px-7 xl:px-8 2xl:px-10"
+    >
+      <div>
+        <a
+          href="/"
+          class="py-4 text-xl leading-3 font-bold font-ptserif text-blueGray-900"
+          >PLOSKER <span class="text-blueGray-500">GROUP</span>
+          <br /><span
+            class="text-xs tracking-wider leading-3 font-ptsans font-bold text-blueGray-500"
+            >ЮРИДИЧЕСКАЯ КОМПАНИЯ</span
+          ></a
+        >
+      </div>
+      <div class="flex items-center self-start leading-3 space-x-4">
+        <a href="https://api.whatsapp.com/send?phone=79781381159" class="font-bold text-blueGray-500 text-xl">+7 978 138-11-59</a>
+      </div>
+    </div>
+    <div class="container-fluid lg:w-1/2">
+      <h1
+        class="text-5xl xl:text-6xl 2xl:text-7xl font-bold text-blueGray-900 pt-8 mb-6 2xl:mb-12"
+      >
+        Специализация&nbsp;&mdash; <br /><span class="text-blueGray-500"
+          >налоговое право</span
+        >
       </h1>
-      <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+      <p class="text-xl xl:text-xl 2xl:text-3xl text-blueGray-500">
+        Налоговая практика для юридических лиц
+        <br />и индивидуальных предпринимателей
       </p>
     </div>
   </div>
-  <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-    <img class="absolute inset-0 w-full h-full object-cover" src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="">
+  <div
+    class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full"
+  >
+    <img
+      class="absolute inset-0 w-full h-full object-cover"
+      src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
+      alt=""
+    />
   </div>
-</main>
-
-
-
-
-
+</div>
 
 <!-- <div class="container-fluid flex">
   <div class="w-1/2 flex items-center h-screen">
